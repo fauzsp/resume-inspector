a02f18745981cd1f291aefe49ceba8fc7f8c14e1
fauzsp <fauz_sp@hotmail.com>
2025-07-25 15:18:41 +0500

    resume cards created on the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Inspector" },
+    { name: "description", content: "Smart resume analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (<main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+      <h1>Welcome to Resume Inspector</h1>
+      <h2>Your smart resume analyzer</h2>
+      </div>
+{resumes.length > 0 &&
+<div className="resumes-section">
+    {resumes.map((resume) => {
+     return <ResumeCard key={resume.id} resume={resume} />
+    })}
+    </div>
+  }
+    </section>
+    </main>
+  )
 }
